ffadf4f395b63047e330f3e9634b59acbba312a0
Christian Hergert <chergert@redhat.com>
2025-05-11 15:01:52 -0700

    tests: port to girepository-2.0
    This requires a GLib with gi_repository_dup_default() which can be gotten
    from GNOME/glib!4630.

--- tests/testing-util/testing-util.c
+++ tests/testing-util/testing-util.c
@@ -26,7 +26,8 @@
 #include <stdlib.h>
 
 #include <glib.h>
-#include <girepository.h>
+
+#include <girepository/girepository.h>
 
 #include "libpeas/peas-engine-priv.h"
 
@@ -189,6 +190,7 @@ void
 testing_util_init (void)
 {
   GError *error = NULL;
+  GIRepository *repository;
   const GDebugKey glib_debug_keys[] = {
     { "fatal-criticals", G_LOG_LEVEL_CRITICAL },
     { "fatal-warnings",  G_LOG_LEVEL_CRITICAL | G_LOG_LEVEL_WARNING }
@@ -209,10 +211,13 @@ testing_util_init (void)
   fatal_flags = g_parse_debug_string (g_getenv ("G_DEBUG"), glib_debug_keys,
                                       G_N_ELEMENTS (glib_debug_keys));
 
-  g_irepository_require_private (g_irepository_get_default (),
+  repository = gi_repository_dup_default ();
+  gi_repository_require_private (repository,
                                  BUILDDIR "/libpeas",
                                  "Peas", API_VERSION_S, 0, &error);
+
   g_assert_no_error (error);
+  g_clear_object (&repository);
 
   initialized = TRUE;
 }
