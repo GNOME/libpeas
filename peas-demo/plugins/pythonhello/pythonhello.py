53657c06fe31871bd3d050c6c5668611f928f8e3
Christian Hergert <chergert@redhat.com>
2025-05-23 16:37:03 -0700

    peas-demo: add gi.require_version() for GTK 3.0

--- peas-demo/plugins/pythonhello/pythonhello.py
+++ peas-demo/plugins/pythonhello/pythonhello.py
@@ -19,6 +19,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
+import gi
+
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import GObject
 from gi.repository import Peas
 from gi.repository import PeasGtk
